450d472842aa278e53c4a21c57a56835942d46ed
Muhammad Arshad <muhammadarshad0910@gmail.com>
2023-11-05 00:41:29 +0000

    dart doesn't allow minification

--- examples/lib/main.dart
+++ examples/lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:capnotrainer_sdk/capnotrainer_sdk.dart';
-
 import 'package:examples/widgets.dart';
 
 void main() {
